f9993a4fe9d83260a0673d3f5754adef46aad1c1
MrBaNkS <banksmi09@gmail.com>
2014-11-10 07:59:49 -0600

    Add vendor overlays
    Partial pick for .png and overlay

--- config/common.mk
+++ config/common.mk
@@ -14,3 +14,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Disable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/androidx/overlay/common
