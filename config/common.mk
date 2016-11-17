a5fd57f835e97fa1292192761d0981c108302523
Harsh Shandilya <msfjarvis@gmail.com>
2016-11-17 11:07:50 +0100

    Add ro.substratum.verified=true to build.prop
    this disables warning from Substratum about incompatible N-MR1/7.1+/API25 firmware
    
    Change-Id: Ie6863697b7fa4326085289ca7ae58c0efe54ec52

--- config/common.mk
+++ config/common.mk
@@ -10,7 +10,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.substratum.verified=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
