34e1a320ca3671eb5c724dc0a37319813a884a28
camcory <camcory1@gmail.com>
2017-08-15 12:54:31 -0400

    Build OmniSubs and OmniTheme

--- config/common.mk
+++ config/common.mk
@@ -4,5 +4,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 
+# Additional packages
+PRODUCT_PACKAGES += \
+    OmniSubs \
+    OmniTheme
+
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/androidx/overlay/common
