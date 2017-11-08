90b02591d9e19899d004e7d718c0b716344ea4e6
Alex Cruz <du.alexcruz@gmail.com>
2017-11-07 21:19:51 -0500

    Rebrand backuptool
    Change-Id: Ie159d3fea2646a41517c03c5927913e807ef4b51

--- build/tools/50-custom.sh
+++ build/tools/50-custom.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # 
-# /system/addon.d/50-cm.sh
-# During a CM9 upgrade, this script backs up /system/etc/hosts,
+# /system/addon.d/50-custom.sh
+# During an upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
