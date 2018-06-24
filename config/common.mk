4d1fc42ceeb682c63dd1624e9a84174677515ee0
camcory <camcory1@gmail.com>
2018-06-24 17:09:16 -0400

    Remove Berkeley

--- config/common.mk
+++ config/common.mk
@@ -23,15 +23,9 @@ PRODUCT_COPY_FILES += \
     vendor/androidx/build/tools/50-custom.sh:system/addon.d/50-custom.sh
 
 # Latin IME lib
-ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/androidx/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/androidx/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
-else
-PRODUCT_COPY_FILES += \
-    vendor/androidx/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/androidx/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-endif
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo androidx_berkeley-userdebug
 add_lunch_combo androidx_shamu-userdebug
