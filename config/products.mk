c97b9453bb51c186ad364ed2dc06bc16baa3509a
camcory <camcory1@gmail.com>
2018-07-17 10:51:04 -0400

    Remove DUI for now

--- config/products.mk
+++ config/products.mk
@@ -8,11 +8,11 @@ PRODUCT_PACKAGES += \
     Launcher3
 
 # DU Utils Library
-PRODUCT_PACKAGES += \
-    org.dirtyunicorns.utils
+#PRODUCT_PACKAGES += \
+#    org.dirtyunicorns.utils
 
-PRODUCT_BOOT_JARS += \
-    org.dirtyunicorns.utils
+#PRODUCT_BOOT_JARS += \
+#    org.dirtyunicorns.utils
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
