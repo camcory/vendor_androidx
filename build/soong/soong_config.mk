341007f35e54daf2e682a06729c286c4101da58d
Christian Oder <myself5@carbonrom.org>
2018-08-07 17:14:46 +0200

    vendor: Rewrite soong_config for Pie
    Change-Id: I71ba2262026dda8688faeff712f57aa6c001c656

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,3 @@
-custom_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+$(eval _contents := $$(_contents)"Custom": {$$(newline))
+
+$(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))
