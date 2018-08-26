fb6127bb682f8e5eee1c31dfb64142f44b83a238
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-25 20:23:19 -0600

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,4 +2,6 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
 $(eval _contents := $$(_contents)"Custom": {$$(newline))
 
+$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+
 $(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))
